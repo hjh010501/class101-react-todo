d222fbf961080a2ebd437aa636e4015c13276139
kidevelop <kidevelop@kaist.ac.kr>
2021-09-11 16:04:53 +0900

    feature: page layout, todo board base - Change NotFound Page location

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,5 +1,42 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eee;
+`;
+
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background: white;
+  box-shadow: 0px 25px 100px -60px rgba(0, 0, 0, 0.18);
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+`;
+
+const TodoList = styled.div``;
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%;
+  font-size: 1.2em;
+  border-bottom: 1px solid #eee;
+`;
+
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
 
 export function HomePage() {
   return (
@@ -8,7 +45,18 @@ export function HomePage() {
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <span>HomePage container</span>
+      <Wrapper>
+        <Box>
+          <Title>할 일</Title>
+
+          <TodoList>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              adad
+            </TodoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
     </>
   );
 }
